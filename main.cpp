3c3e930ea158d88a2486b87cafca2a94bbad8365
Greg Saive <greg@evias.be>
2013-01-11 14:42:33 +0100

    add array range based for-loop unitary test to test suite.

--- main.cpp
+++ main.cpp
@@ -19,15 +19,18 @@ int main(int argc, char** argv)
     using unit_test::test_range_based_for;
     using std::map;
     using std::vector;
+    using std::array;
     using std::string;
 
     using test_map_rbfor    = test_range_based_for<map<string,string>>;
     using test_vector_rbfor = test_range_based_for<vector<string>>;
+    using test_array_rbfor  = test_range_based_for<array<string,3>>;
 
     /* Instanciate the unitary test suite. */
     auto thesuite = new unit_test::suite({
         new test_map_rbfor("Range based For-loop <map<string, string>>"),
-        new test_vector_rbfor("Range based For-loop <vector<string>>")});
+        new test_vector_rbfor("Range based For-loop <vector<string>>"),
+        new test_array_rbfor("Range based For-loop <array<string,3>>")});
 
     thesuite->run();
     return 0;
